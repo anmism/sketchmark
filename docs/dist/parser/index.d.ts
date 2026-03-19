da4d1056366bc51d10cfaa6f55a2cf00f080dbbb
BapunHansdah <111235988+BapunHansdah@users.noreply.github.com>
2026-03-19 14:02:39 +0530

    remove-dist-and-use-published-npm

--- docs/dist/parser/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { DiagramAST } from "../ast/types";
-export type { DiagramAST } from "../ast/types";
-export declare class ParseError extends Error {
-    line: number;
-    col: number;
-    constructor(msg: string, line: number, col: number);
-}
-export declare function parse(src: string): DiagramAST;
-//# sourceMappingURL=index.d.ts.map
